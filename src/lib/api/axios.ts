ccb689ce3c44c2a3b222b322c32f1b5757c2ceb6
chordzz <olaifeolawore@gmail.com>
2025-06-21 07:32:07 +0100

    fixed env

--- src/lib/api/axios.ts
+++ src/lib/api/axios.ts
@@ -3,10 +3,12 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
-    // headers: {
-    //     'Content-Type': 'application/json',
-    // },
+  baseURL: process.env.NODE_ENV === 'production'
+    ? process.env.API_BASE_URL
+    : process.env.NEXT_PUBLIC_API_BASE_URL,
+  // headers: {
+  //     'Content-Type': 'application/json',
+  // },
 })
 
 axiosInstance.interceptors.request.use( config => {
